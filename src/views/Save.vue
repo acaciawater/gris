c038487193c45e25134fa37c47534946726be387
Theo Kleinendorst <theo.kleinendorst@acaciawater.com>
2019-03-08 17:34:31 +0100

    reformatting

--- src/views/Save.vue
+++ src/views/Save.vue
@@ -21,50 +21,50 @@
 </template>
 
 <script>
-  export default {
-    name: 'save',
-    data () {
-      return {
-        project_name: '',
-        googleLocation: JSON.parse(localStorage.getItem('googleLocation')) || ''
+export default {
+  name: 'save',
+  data () {
+    return {
+      project_name: '',
+      googleLocation: JSON.parse(localStorage.getItem('googleLocation')) || ''
+    }
+  },
+  methods: {
+    save () {
+      console.log(JSON.stringify(this.$store.state.survey))
+      console.log(JSON.stringify(this.$store.state.map.location))
+      let survey = JSON.stringify(this.$store.state.survey)
+      let lat = this.$store.state.map.location[0]
+      let lng = this.$store.state.map.location[1]
+      let data = {
+        project: this.project_name,
+        location: {
+          type: 'Point',
+          coordinates: [lng, lat]
+        },
+        survey: JSON.parse(survey)
       }
-    },
-    methods: {
-      save () {
-        console.log(JSON.stringify(this.$store.state.survey))
-        console.log(JSON.stringify(this.$store.state.map.location))
-        let survey = JSON.stringify(this.$store.state.survey)
-        let lat = this.$store.state.map.location[0]
-        let lng = this.$store.state.map.location[1]
-        let data = {
-          project: this.project_name,
-          location: {
-            type: 'Point',
-            coordinates: [lng, lat]
-          },
-          survey: JSON.parse(survey)
-        }
-        // console.log(data)
-        localStorage.setItem('survey', JSON.stringify(data))
-        let token = sessionStorage.getItem('token')
-        let config = { headers: { Authorization: 'JWT ' + token } }
-        this.$http
-          .post('/api/v1/survey/', data, config)
-          .then(response => {
-            console.debug('Successfully saved!')
-            console.debug(response)
-          })
-          .catch(error => {
-            let e = error.response.data
-            let errormsg = ''
-            for (const key in e) {
-              errormsg += key + ': ' + e[key].toString()
-            }
-            console.error(error.response.statusText, errormsg)
-          })
-      }
+      // console.log(data)
+      localStorage.setItem('survey', JSON.stringify(data))
+      let token = sessionStorage.getItem('token')
+      let config = { headers: { Authorization: 'JWT ' + token } }
+      this.$http
+        .post('/api/v1/survey/', data, config)
+        .then(response => {
+          console.debug('Successfully saved!')
+          console.debug(response)
+        })
+        .catch(error => {
+          let e = error.response.data
+          let errormsg = ''
+          for (const key in e) {
+            errormsg += key + ': ' + e[key].toString()
+          }
+          console.error(error.response.statusText, errormsg)
+        })
     }
   }
+}
 </script>
 
 <style scoped>
